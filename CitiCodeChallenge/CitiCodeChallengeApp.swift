ab4f2f7aaeeb54472493f537fa5960b731461ba1
JL <j.luis.enriquez77@gmail.com>
2024-02-22 12:59:10 -0600

    Initial Commit

---
+++ CitiCodeChallenge/CitiCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiCodeChallengeApp.swift
+//  CitiCodeChallenge
+//
+//  Created by JL on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CitiCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
